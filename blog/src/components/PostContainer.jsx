daee0336c9ce04f45cf6e3e6dea0c7558371afff
César <cesarlpb@gmail.com>
2023-07-27 18:46:11 +0200

    fetch para comprobar si el server está activo

--- blog/src/components/PostContainer.jsx
+++ blog/src/components/PostContainer.jsx
@@ -3,18 +3,40 @@ import Post from './Post';
 import json from '../assets/json/posts.json';
 
 const PostContainer = (props) => {
-  const {sectionId, id, title} = props
-  const [posts, setPosts] = useState([])
+  const {sectionId, id, title} = props;
+  const [posts, setPosts] = useState([]);
+  const [isServerActive, setIsServerActive] = useState(false);
   const rand = () => {
     return `key-${parseInt(Math.random()*1000)}`
   }
   useEffect(() => {
-  {/* 1. Leer el JSON y seleccionar un section a partir del id que me pasan por props */}
-  // id -> identificador de seccion (1, 2, 3)
-  const section = json.filter(section => section.id == id) // Filtramos el section que tiene el id que nos han pasado
+  {
+    const ip = "79.143.92.203";
+    const port = "3000"
+    const testEndpoint = `https://${ip}:${port}/`
 
-  // Observación: json.filter(...) devuelve un array.
-  setPosts(section[0].posts) // Actualizamos el estado de los posts. Accedemos al array de posts de este section
+    fetch(testEndpoint)
+      .then(res => {
+        console.log("status code:", res.status)
+        if(res.status == 200){
+          setIsServerActive(true)
+        }else{
+          setIsServerActive(false)
+        }
+        return res.text()
+      })
+      .then(texto => console.log("texto:", texto))
+  // Si el servidor no está activo -> leemos JSON
+  // Si el servidor ESTÁ activo -> pedimos posts
+  if(isServerActive){
+    // fetch de posts
+  }else{
+    /* 1. Leer el JSON y seleccionar un section a partir del id que me pasan por props */}
+    // id -> identificador de seccion (1, 2, 3)
+    const section = json.filter(section => section.id == id) // Filtramos el section que tiene el id que nos han pasado
+    // Observación: json.filter(...) devuelve un array.
+    setPosts(section[0].posts) // Actualizamos el estado de los posts. Accedemos al array de posts de este section
+  }
   
   }, []) // [] -> no hay condición para volver a ejecutar el useEffect(...)
   return (

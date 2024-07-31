08dc2f7d705c61e501f0d0b9ce8d3a233730f6b3
César <cesarlpb@gmail.com>
2024-07-31 17:56:11 +0200

    Reubico página index en componente Home

---
+++ react-vite/src/Home.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function Home(){
+
+  const [count, setCount] = useState(0)
+  const incrementarContador = () => setCount((count) => count - 1)
+  const decrementarContador = () => setCount((count) => count + 1)
+
+  useEffect(() => {
+    // console.log("Ha cargado el componente. Hola, desde useEffect")
+    if (count !== 0) {
+        console.log("Ha cambiado count, ahora vale:", count);
+    }
+    return;
+  }, [count]);
+
+  return (
+    <>
+    <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+    </div>
+    <h1>Contador:</h1>
+    <div className="card">
+      <button onClick={incrementarContador}>
+        restar 1: {count}
+      </button>
+      &nbsp;
+      <button onClick={decrementarContador}>
+        sumar 1: {count}
+      </button>
+    </div>
+    </>
+    )
+}
+
+export default Home

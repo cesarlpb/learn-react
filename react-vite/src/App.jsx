8cb75c8e98cc7b39667a12e52327f923d622bbd1
César <cesarlpb@gmail.com>
2024-07-31 16:06:58 +0200

    Añado decremento del contador

--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -16,18 +16,16 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Contador:</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setCount((count) => count - 1  )}>
+          restar 1: {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        &nbsp;
+        <button onClick={() => setCount((count) => count + 1  )}>
+          sumar 1: {count}
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }

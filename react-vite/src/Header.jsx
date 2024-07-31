f79508becdb416c69114ae49e098449a585512c3
César <cesarlpb@gmail.com>
2024-07-31 18:17:41 +0200

    Link para links

--- react-vite/src/Header.jsx
+++ react-vite/src/Header.jsx
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom";
+
 function Header(){
   return (
     <>
-      <div>1</div>
-      <div>2</div>
+      <Link to="/">Inicio</Link>  
+      <Link to="acerca-de">Acerca de</Link>
+        
+      {/* Si usamos <a> recarga toda la página y los componentes se resetean
+      y los estado TAMBIÉN (ojo) => es mejor usar Link en React para los enlaces
+      - Es fancy? True */}
+
+      {/* <a href="acerca-de">Acerca de</a> */}
+
+      <Link to="otro-link">Otro Link</Link>
     </>
   )
 }
